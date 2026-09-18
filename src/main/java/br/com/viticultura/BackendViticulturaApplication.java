694bb990c7012503ac36509612dde6e9d103dc6a
Priscila <priscilabsmar@gmail.com>
2026-09-17 22:36:45 -0300

    feat: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/br/com/viticultura/BackendViticulturaApplication.java
@@ -0,0 +1,13 @@
+package br.com.viticultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendViticulturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendViticulturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viticultura/BackendViticulturaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viticultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendViticulturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
